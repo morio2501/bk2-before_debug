144286f977f9eedf83bf664044abcd6fd9177e4d
vagrant <morio4899@gmail.com>
2020-08-03 00:22:16 +0000

    instans

--- bk2-before_debug/app/controllers/books_controller.rb
+++ bk2-before_debug/app/controllers/books_controller.rb
@@ -2,47 +2,49 @@ class BooksController < ApplicationController
       before_action :authenticate_user!
 
   def show
-    @book = Book.find(params[:id])
+      @book = Book.find(params[:id])
   end
 
   def index
-    @books = Book.all
-    @book = Book.new
+      @books = Book.all
+      @book = Book.new
   end
 
   def create
-      @book = Book.new(book_params)
-    if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+        @book = Book.new(book_params)
+        @book.user_id = current_user.id
+    if  @book.save
+        redirect_to book_path(@book), notice: "You have created book successfully."
     else
-      @books = Book.all
-      render 'index'
+        @book.user_id = current_user.id
+        @books = Book.all
+        render "index"
     end
   end
 
   def edit
-    @book = Book.find(params[:id])
+        @book = Book.find(params[:id])
   end
 
   def update
-    @book = Book.find(params[:id])
-    if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+        @book = Book.find(params[:id])
+    if  @book.update(book_params)
+        redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render "edit"
+        render "edit"
     end
   end
 
   def destoy
-    @book = Book.find(params[:id])
-    @book.destoy
-    redirect_to books_path
+      @book = Book.find(params[:id])
+      @book.destoy
+      redirect_to books_path
   end
 
     private
 
     def book_params
-      params.require(:book).permit(:title)
+      params.require(:book).permit(:title, :body)
    end
 
 end
--- bk2-before_debug/app/controllers/users_controller.rb
+++ bk2-before_debug/app/controllers/users_controller.rb
@@ -17,9 +17,9 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
+      @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user.id), notice: "You have updated user successfully."
+      redirect_to user_path(@user), notice: "You have updated user successfully."
     else
       render "edit"
     end
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
-
+  
   def ensure_correct_user
     @user = User.find(params[:id])
     unless @user == current_user
